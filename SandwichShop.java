3b97252b88f9a4ecd9036770297446e0305ca3c1
Tori Stake <42363446+extratoristrial@users.noreply.github.com>
2019-02-09 11:03:28 -0600

    Add files via upload
    fixed indentation

--- SandwichShop.java
+++ SandwichShop.java
@@ -19,13 +19,19 @@ public class SandwichShop
         out.println("How many veggie sandwiches were sold today?");
         veggiesToday = keyboard.nextInt();
         out.println("You sold " + veggiesToday + " sandwiches.");
-        if (veggiesToday == goalForVeggies) {
+        if (veggiesToday == goalForVeggies)
+        {
             out.println("Meets goal!");
-        } else if (veggiesToday < goalForVeggies) {
+        }
+        else if (veggiesToday < goalForVeggies)
+        {
             out.println("Fell short");
-        } else if (veggiesToday > goalForVeggies){
+        }
+        else if (veggiesToday > goalForVeggies)
+        {
             out.println("Surpassed goal! Good job!");
-        };
+        }
+        ;
 
         //burgers
         int burgersToday;
@@ -33,13 +39,19 @@ public class SandwichShop
         out.println("How many burgers were sold today?");
         burgersToday = keyboard.nextInt();
         out.println("You sold " + burgersToday + " burgers.");
-        if (burgersToday == goalForBurgers) {
+        if (burgersToday == goalForBurgers)
+        {
             out.println("Meets goal!");
-        } else if (burgersToday < goalForBurgers) {
+        }
+        else if (burgersToday < goalForBurgers)
+        {
             out.println("Fell short");
-        } else if (burgersToday > goalForBurgers){
+        }
+        else if (burgersToday > goalForBurgers)
+        {
             out.println("Surpassed goal! Good job!");
-        };
+        }
+        ;
 
         //subs
         int subsToday;
@@ -47,13 +59,19 @@ public class SandwichShop
         out.println("How many subs were sold today?");
         subsToday = keyboard.nextInt();
         out.println("You sold " + subsToday + " subs.");
-        if (subsToday == goalForSubs) {
+        if (subsToday == goalForSubs)
+        {
             out.println("Meets goal!");
-        } else if (subsToday < goalForSubs) {
+        }
+        else if (subsToday < goalForSubs)
+        {
             out.println("Fell short");
-        } else if (subsToday > goalForSubs){
+        }
+        else if (subsToday > goalForSubs)
+        {
             out.println("Surpassed goal! Good job!");
-        };
+        }
+        ;
 
         //soup
         int soupToday;
@@ -61,18 +79,27 @@ public class SandwichShop
         out.println("How many soups were sold today?");
         soupToday = keyboard.nextInt();
         out.println("You sold " + soupToday + " soups.");
-        if (soupToday == goalForSoup) {
+        if (soupToday == goalForSoup)
+        {
             out.println("Meets goal!");
-        } else if (soupToday < goalForSoup) {
+        }
+        else if (soupToday < goalForSoup)
+        {
             out.println("Fell short");
-        } else if (soupToday > goalForSoup){
+        }
+        else if (soupToday > goalForSoup)
+        {
             out.println("Surpassed goal! Good job!");
-        };
+        }
+        ;
 
-        if (veggiesToday == goalForVeggies && burgersToday == goalForBurgers && subsToday == goalForSubs && soupToday == goalForSoup) {
+        if (veggiesToday == goalForVeggies && burgersToday == goalForBurgers && subsToday == goalForSubs && soupToday == goalForSoup)
+        {
             out.println("Made goal for everything!");
-        } else if (veggiesToday > goalForVeggies && burgersToday > goalForBurgers && subsToday > goalForSubs && soupToday > goalForSoup) {
+        }
+        else if (veggiesToday > goalForVeggies && burgersToday > goalForBurgers && subsToday > goalForSubs && soupToday > goalForSoup)
+        {
             out.print("Surpassed goal for everything! Wow! Good job!");
-    }
+        }
     }
 }
